6481482063f52aabf625099efcd5c334dc1d72dc
GibboK <gibbok.coding@gmail.com>
2023-04-25 08:58:59 +0200

    render

--- apps/apps/web/pages/index.tsx
+++ apps/apps/web/pages/index.tsx
@@ -1,10 +1,30 @@
 import { Button } from "ui";
 
+import { useQuery, gql } from '@apollo/client';
+
+
+const GET_BOOKS = gql`
+  query GetBooks {
+    books {
+      author
+      title
+    }
+  }
+`;
+
 export default function Web() {
+  const { loading, error, data } = useQuery(GET_BOOKS);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error :(</p>;
+  }
+
   return (
     <div>
-      <h1>Web</h1>
-      <Button />
+      {data.books.map(x => <div key={x.id}>{JSON.stringify(x)}</div>)}
     </div>
   );
 }
