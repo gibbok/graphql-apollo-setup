35f8bf3b76165fc71b1b85fa138dfd3af5eee6fc
GibboK <gibbok.coding@gmail.com>
2023-04-27 09:00:19 +0200

    transform

--- apps/apps/web/pages/_app.tsx
+++ apps/apps/web/pages/_app.tsx
@@ -1,9 +1,33 @@
 import type { AppProps } from 'next/app'
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink, ApolloLink, Operation, NextLink } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client";
 
+const BASE_URI = "http://localhost:4000/"
+
+const httpLink = new HttpLink({ uri: BASE_URI });
+
+const transformResponse = (operation: Operation, forward: NextLink) => {
+    return forward(operation).map(response => {
+        if (response.data) {
+            return {
+                ...response,
+                data: {
+                    books: response.data.books.map(x => ({
+                        ...x,
+                        title: x.title.toUpperCase() + ' XXX'
+                    }))
+                }
+            };
+        } else {
+            return response;
+        }
+    });
+};
+
+const link = ApolloLink.from([transformResponse, httpLink]);
+
 const client = new ApolloClient({
-    uri: "http://localhost:4000/",
+    link,
     cache: new InMemoryCache(),
 });
 
