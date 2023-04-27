c57ca4cc8bc7c7ca4b9586f3463f7369192ff74c
GibboK <gibbok.coding@gmail.com>
2023-04-27 16:12:29 +0200

    save

--- apps/apps/web/pages/readers.tsx
+++ apps/apps/web/pages/readers.tsx
@@ -1,11 +1,7 @@
-import { useQuery } from '@apollo/client';
-import { BooksUI, ResultBooksUI } from '../ui-types';
-import { GetBooksDocument, GetReadersDocument, useGetReadersQuery } from '../../server/src/__generated__/queries';
-// import { useGetBooksQuery } from '../../server/src/__generated__/queries'
+import { useGetReadersQuery } from '../../server/src/__generated__/queries';
 
 export default function Web() {
   const { loading, error, data } = useGetReadersQuery(); 
-  console.log(data)
 
   if (loading) {
     return <p>Loading...</p>;
